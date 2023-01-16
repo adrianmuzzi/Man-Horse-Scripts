3966c6f1a7f406e550b5020ec90781b8b109b8e0
Barclay-McClay <102266976+Barclay-McClay@users.noreply.github.com>
2023-01-16 16:55:15 +1100

    Update Serpentine.sh

--- Dev/Serpentine.sh
+++ Dev/Serpentine.sh
@@ -3,11 +3,13 @@ GREEN='\033[0;32m'
 NOCOLOR='\033[0m'
 echo -e "${GREEN}Running SERPENTINE${NOCOLOR}"
 system_profiler SPSoftwareDataType SPHardwareDataType
-export HISTIGNORE='*sudo -S*' #make sure we dont save the password in ~/.bash_history
-export HISTIGNORE='*admin_pass*' #make sure we dont save the password in ~/.bash_history
+#####################################################
+#export HISTIGNORE='*sudo -S*' #make sure we dont save the password in ~/.bash_history
+#export HISTIGNORE='*admin_pass*' #make sure we dont save the password in ~/.bash_history
 #echo "Enter your local user password:"
 #read -r admin_pass
 #echo "${admin_pass}" | sudo -S -k <do thing>
+#####################################################
 installed_apps=$(mdfind "kMDItemKind == 'Application'")
 if [[ "$installed_apps" == *"Google Chrome"* ]]; then
     echo -e "${GREEN}Google Chrome was already detected on this system.${NOCOLOR}"
@@ -33,6 +35,13 @@ else
     open /Applications
 fi
 echo -e "${GREEN}Chrome Install Complete.${NOCOLOR}"
-
+#####################################################
 echo "User's Microsoft Portal opening in Chrome..."
 open -a "Google Chrome" https://portal.office.com
+#####################################################
+echo "Backing up User's dock..."
+cp -af  ~/Library/Preferences/com.apple.dock.plist ~/Library/Preferences/com.apple.dock.plist.bak
+echo "Clearing user's dock..."
+defaults delete com.apple.dock persistent-apps; killall Dock
+echo "Opening applications folder..."
+open /Applications
