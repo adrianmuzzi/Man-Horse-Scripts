04bf8094bf945cf3e479825101da4041e49de65c
adrianmuzzi <adrianjamesmuzzi@gmail.com>
2023-05-02 08:37:15 +1000

    Revert "Update Serpentine"
    This reverts commit b7a945c4f9fb43ee87d56d981705d6a30d982f27.

--- Serpentine/Serpentine.sh
+++ Serpentine/Serpentine.sh
@@ -21,16 +21,23 @@ installed_apps=$(mdfind "kMDItemKind == 'Application'")
 if [[ "$installed_apps" == *"Google Chrome"* ]]; then
     echo -e "${GREEN}Google Chrome was already detected on this system.${NOCOLOR}"
 else
-    # Download the latest version of Google Chrome
-    curl -O https://dl.google.com/chrome/mac/stable/GGRO/googlechrome.dmg
-    # Mount the downloaded DMG file
-    hdiutil attach googlechrome.dmg
-    # Install Google Chrome
-    sudo cp -r /Volumes/Google\ Chrome/Google\ Chrome.app /Applications
-    # Unmount the DMG file
-    hdiutil detach /Volumes/Google\ Chrome
-    # Remove the DMG file
-    rm googlechrome.dmg
+    if [ ! -d ~/Downloads/Serpentine/ ]; then
+        mkdir ~/Downloads/Serpentine/
+    fi
+    #install Chrome (checking which version is appropriate for Apple or Intel chipset)
+    echo "Downloading and installing Google Chrome..."
+    if [[ $(arch) == arm64 ]]; then
+            echo "Architecture is Apple ARM"
+            curl https://dl.google.com/chrome/mac/stable/ --output ~/Downloads/Serpentine/googlechrome.dmg
+    else
+            echo "Architecture is Intel X86"
+            curl https://dl.google.com/chrome/mac/universal/stable/GGRO/googlechrome.dmg --output ~/Downloads/Serpentine/googlechrome.dmg
+    fi
+    #install chrome from the downloaded .dmg
+    yes | hdiutil attach -noverify -nobrowse -mountpoint ~/Downloads/Serpentine/mount ~/Downloads/Serpentine/googlechrome.dmg
+    cp -r ~/Downloads/Serpentine/mount/*.app /Applications
+    hdiutil detach ~/Downloads/Serpentine/mount
+    open /Applications
 fi
 echo -e "${GREEN}Chrome Install Complete.${NOCOLOR}"
 #####################################################
